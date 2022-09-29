10bbc5e65bb5113db7846e6bb6376640752c28a4
Michael Quick <mwquick04@gmail.com>
2022-09-29 11:43:46 -0400

    Completed lab 2

--- lab02.py
+++ lab02.py
@@ -1,3 +1,4 @@
+# 1) Write code
 age = int(input("Enter your age: "))
 if age >= 9:
     height = float(input("Enter your height in cm: "))
@@ -8,15 +9,20 @@ if age >= 9:
 else:
     print("You are too young for this ride.")
 
+# 2) Fix bugs in code
+
 IDEAL_CREDIT_SCORE = 720
 
 userScore = int(input("Please enter your credit score: "))
 housePrice = int(input("Please enter the price of the house: "))
 
+# => should be >=
 if userScore >= IDEAL_CREDIT_SCORE:
     downPayment = 0.1 * housePrice
-elif IDEAL_CREDIT_SCORE > userScore > 600:
+# else if should be elif & "600" should be 600
+elif userScore < IDEAL_CREDIT_SCORE and userScore > 600:
     downPayment = 0.2 * housePrice
 else:
+    # Anything inside else statement must be indented
     downPayment = 0.3 * housePrice
 print("Your down payment is: ${}".format(downPayment))
