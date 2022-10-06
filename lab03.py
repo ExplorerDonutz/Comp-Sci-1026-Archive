db870b997b98d63ea4813237ddf5d1934189e0dd
Michael Quick <mwquick04@gmail.com>
2022-10-06 11:46:51 -0400

    Completed lab 3

---
+++ lab03.py
@@ -0,0 +1,32 @@
+n = int(input("How many numbers do you want to use today? "))
+if n > 0:
+    firstValue = int(input("Enter the first number: "))
+    largest = firstValue
+    smallest = firstValue
+    total = firstValue
+
+    counter = 0
+    while counter < (n-1):
+        current = int(input("Enter the next number: "))
+        total = total+current
+        counter = counter+1
+        if current < smallest:
+            smallest = current
+        elif current > largest:
+            largest = current
+    print("The average of the values is: ", total/n)
+    print("The smallest of the values is {}".format(smallest))
+    print("The largest of the values is {}".format(largest))
+    print("The range of the values is {}".format(largest-smallest))
+else:
+    print("You did not want to use any numbers today.")
+
+accountTotal = 50
+# Should be >, not =<
+while accountTotal > 20:
+    # Needs to take 1 away each loop
+    accountTotal -= 1
+    print(accountTotal)
+
+
+print("Your account has reached $20")
